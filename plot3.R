1325175c7b9fdd4adb790cacd5e36253b3adbd3a
unknown <owengsaguibo@gmail.com>
2014-05-08 16:32:40 -0700

    add plot3 modify plot2.R comment from Histogram to Plot

--- plot3.R
+++ plot3.R
@@ -11,8 +11,18 @@ hpc <- read.table("household_power_consumption.txt",
 ##Create string of Date and Time
 Days <- strptime(paste(hpc$Date, hpc$Time), format='%d/%m/%Y %H:%M:%S')
 
-##Create Line Plot
-plot(Days, hpc$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+## Plot Sub_metering_1 VS Days
+ plot(Days, hpc[,7], type="l", xlab="", ylab="Energy sub metering")
+
+## Plot Sub_metering_2 VS Days
+lines(Days, hpc[,8], type="l", col="red")
+
+## Plot Sub_metering_3 VS Days
+lines(Days, hpc[,9], type="l", col="blue")
+
+## Adds Legends
+legend("topright", lty=c(1,1), col=c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
 
 ##Save Plot in png format
 dev.copy(png, "plot2.png")
